d5cbc1ff6401cecd5f651ec67ccebcaf1770e67f
phillakt <phillakt@gmail.com>
2021-05-28 19:27:20 +0300

    create 3 pages

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './scss/main.scss';
+
+// section scss
+// import '../src/scss/section/_header.scss';
+// import '../src/scss/section/_breadcrumbs.scss';
+import '../src/scss/section/_auth.scss';
+import '../src/scss/section/_list.scss';
+// import '../src/scss/section/_sidebar.scss';
+
+
+// modules js
+import index from './js/app';
+
